ad68b4b56552be2cb4c80627ff79bbae7bb5a0a2
Matthew Hemingway <dpplrdr435@gmail.com>
2020-09-13 11:37:27 -0400

    add publisher relationships

--- src/main/java/com/matthem/spring5webapp/domain/Book.java
+++ src/main/java/com/matthem/spring5webapp/domain/Book.java
@@ -7,6 +7,7 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -19,6 +20,9 @@ public class Book {
   private String title;
   private String isbn;
 
+  @ManyToOne
+  private Publisher publisher;
+
   @ManyToMany
   @JoinTable(name = "author_book", joinColumns = @JoinColumn(name = "book_id"),
       inverseJoinColumns = @JoinColumn(name = "author_id"))
@@ -30,6 +34,14 @@ public class Book {
   public Book(String title, String isbn) {
     this.title = title;
     this.isbn = isbn;
+  }
+
+  public Publisher getPublisher() {
+    return publisher;
+  }
+
+  public void setPublisher(Publisher publisher) {
+    this.publisher = publisher;
   }
 
   public Long getId() {
